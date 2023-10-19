63a43799c180fad73a9622f77b77c0ab818f21f1
fatimazaarate <fatima.zaarate@gmail.com>
2023-10-19 22:19:59 +0100

    maybe done

--- mod.c
+++ mod.c
@@ -29,27 +29,3 @@ void module(stack_t **stack, unsigned int ln)
 	pop(stack, ln);
 	(*stack)->n = result;
 }
-
-/*
-void mod(stack_t **stack, unsigned int line_cnt)
-{
-	int result;
-
-	if (!stack || !*stack || !((*stack)->next))
-	{
-		fprintf(stderr, "L%d: can't mod, stack too short\n", line_cnt);
-		exit(EXIT_FAILURE);
-		return;
-	}
-	if (((*stack)->n) == 0)
-	{
-		fprintf(stderr, "L%d: division by zero\n", line_cnt);
-		exit(EXIT_FAILURE);
-		return;
-	}
-
-	result = ((*stack)->next->n) % ((*stack)->n);
-	pop(stack, line_cnt);
-	(*stack)->n = result;
-} 
-*/
