faa4f63d497a4cf70d791fa9d2f042540230eef0
fatimazaarate <fatima.zaarate@gmail.com>
2023-10-19 17:39:13 +0100

    pint handling

--- stack_oprations.c
+++ stack_oprations.c
@@ -41,3 +41,13 @@ void _pall(stack_t **stack, unsigned int line_number)
 		tmp = tmp->next;
 	}
 }
+
+void _pint(stack_t **stack, unsigned int line_number)
+{
+	if (!(*stack))
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
